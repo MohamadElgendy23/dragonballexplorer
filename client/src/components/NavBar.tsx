fb572cdf0f910c12b6dbfb199b4e4748d144f6ae
Mohamad Elgendy <mohamadelgendy13@gmail.com>
2025-03-19 17:29:03 -0600

    added some responsive design for landing page.

--- client/src/components/NavBar.tsx
+++ client/src/components/NavBar.tsx
@@ -6,7 +6,7 @@ function NavBar() {
         <a href="/" className="cursor p-2 pl-7">
             <img src="https://m.media-amazon.com/images/I/81Z-WeVM-sS.jpg" alt="dragon ball logo" className="rounded-[50%] w-20 h-20"/>
         </a>
-        <button className="font-sans min-w-40 bg-amber-300 shadow-none text-white pt-1.5 pb-1.5 pl-4 pr-4 mr-10 rounded-sm text-2xl leading-6 tracking-wide cursor-pointer hover:bg-amber-400">Docs</button>
+        <a href="https://web.dragonball-api.com/documentation" target="_blank" className="font-sans min-w-40 bg-amber-300 shadow-none text-white pt-1.5 pb-1.5 pl-4 pr-4 mr-10 rounded-sm text-2xl text-center leading-6 tracking-wide cursor-pointer hover:bg-amber-400">Docs</a>
     </nav>
   )
 }
--- client/src/pages/Landing.tsx
+++ client/src/pages/Landing.tsx
@@ -1,10 +1,17 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 
 function Landing() {
+  useEffect(() => {
+}, [])
+    
   return (
     <div className="flex flex-col items-center bg-[#272b33] min-w-full min-h-screen">
-        <div className="flex">
-            <img src="https://images.justwatch.com/poster/178627048/s332/dragon-ball" alt="dragon ball image" />
+        <div className="flex flex-col items-center justify-center pt-4 pb-4 pl-4 pr-4 sm:pt-16 sm:pb-16 gap-5">
+            <img src="https://images.justwatch.com/poster/178627048/s332/dragon-ball" alt="dragon ball image" className="w-150 h-150" />
+            <h1 className="text-5xl text-amber-300 font-black font-[tahoma]">Dragon Ball Explorer</h1>
+        </div>
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 p-4 gap-8 ml-auto mr-auto max-w-[1400px] content-center items-center">
+
         </div>
     </div>
   )

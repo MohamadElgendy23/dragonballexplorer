e2967d78d52c58f39949b188cf7b5b433eebba7a
Mohamad Elgendy <mohamadelgendy13@gmail.com>
2025-03-24 02:07:48 -0600

    added hover effect to character image.

--- client/src/pages/Landing.tsx
+++ client/src/pages/Landing.tsx
@@ -20,12 +20,7 @@ function Landing() {
             <h1 className="text-5xl text-amber-300 font-black font-[tahoma]">Dragon Ball Explorer</h1>
         </div>
         <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 p-4 gap-8 ml-auto mr-auto max-w-[1400px] content-center items-center">
-            {characters.length && characters.map((character, index) => {
-                if (index > 0 && index % 4 === 0)
-                {
-                    setCharactersLoading(true);
-                    return;
-                }
+            {characters.map((character, index) => {
                 return <Character key={index} {...character} />
             })}
         </div>
--- server/routes/dragonballRoutes.ts
+++ server/routes/dragonballRoutes.ts
@@ -1,15 +1,13 @@
 import axios, {AxiosResponse} from "axios";
 import express, {Request, Response} from "express";
 export const dragonballRouter = express.Router();
-import {shuffleArray} from "../helpers/shuffle"
 
 const baseURL = "https://dragonball-api.com/api";
 
 dragonballRouter.get("/characters", async (req: Request, res: Response) => {
     try {
         const response: AxiosResponse = await axios.get(`${baseURL}/characters`);
-        const charactersData = await response.data;
-        shuffleArray(charactersData.items);
+        const charactersData = await response.data.items;
         res.status(200).send(charactersData.items);
     }
     catch (error)

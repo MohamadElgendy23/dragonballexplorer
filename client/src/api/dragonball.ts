58edd56bb40aadd504ca2f2f4ac196558d66b310
Mohamad Elgendy <mohamadelgendy13@gmail.com>
2025-03-25 11:57:09 -0600

    update dragonball.ts and remove unused functions.

--- client/src/api/dragonball.ts
+++ client/src/api/dragonball.ts
@@ -7,34 +7,26 @@ import axios, { AxiosResponse } from "axios";
 const baseURL = "http://localhost:3000/dragonball";
 
 // gets all characters
-export async function getCharacters(page:Number, limit:Number) {
-    try {
-        const response: AxiosResponse = await axios.get(`${baseURL}/characters?page=${page}&limit=${limit}`);
-        const charactersData = await response.data;
-        return charactersData;
-      } catch (error) {
-        console.error(error);
-      }
+export async function getCharacters(page: Number, limit: Number) {
+  try {
+    const response: AxiosResponse = await axios.get(
+      `${baseURL}/characters?page=${page}&limit=${limit}`
+    );
+    const charactersData = await response.data;
+    return charactersData;
+  } catch (error) {
+    console.error(error);
+  }
 }
-
-// gets one character by id
-export async function getCharacter(characterId:String) {
-
-}
-
 // gets all planets
-export async function getPlanets(page:Number, limit:Number) {
-    try {
-        const response: AxiosResponse = await axios.get(`${baseURL}/planets?page=${page}&limit=${limit}`);
-        const planetsData = await response.data;
-        return planetsData;
-      } catch (error) {
-        console.error(error);
-      }
+export async function getPlanets(page: Number, limit: Number) {
+  try {
+    const response: AxiosResponse = await axios.get(
+      `${baseURL}/planets?page=${page}&limit=${limit}`
+    );
+    const planetsData = await response.data;
+    return planetsData;
+  } catch (error) {
+    console.error(error);
+  }
 }
-
-// gets one planet by id
-export async function getPlanet(planetId:String) {
-
-}
-
